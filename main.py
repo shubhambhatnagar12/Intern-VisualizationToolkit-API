c84812345f14d947953e84188d9b231192334bd5
Shubham Bhatnagar <45937476+shubhambhatnagar12@users.noreply.github.com>
2021-06-29 18:32:36 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -12,20 +12,20 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
 
-origins = [
-    #"http://localhost",
-    #"http://localhost:8000"
-    #"http://localhost:8080",
-'*']
+# origins = [
+#     #"http://localhost",
+#     #"http://localhost:8000"
+#     #"http://localhost:8080",
+# '*']
 
-# This completely screws up CORS and is highly insecure but excellent for prototyping
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# # This completely screws up CORS and is highly insecure but excellent for prototyping
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 
 @app.get("/")
